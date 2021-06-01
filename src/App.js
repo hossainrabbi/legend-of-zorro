10e8fe4a3187d4a641c7ded8e6e6d2fc3366f056
Hossain Rabbi <hossainrabbi2k1@gmail.com>
2021-06-01 15:24:55 +0600

    bank balance ui end

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import BankBalance from './components/BankBalance';
 
-const App = () => <div>hello</div>;
+const App = () => (
+    <div className="App d-flex justify-content-center align-items-center mx-auto w-50">
+        <BankBalance />
+    </div>
+);
 
 export default App;
---
+++ src/components/BankBalance.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const BankBalance = () => <div className="bank-balance text-center w-100">123</div>;
+
+export default BankBalance;
